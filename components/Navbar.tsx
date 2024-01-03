a3acebfdbff695844432a5aa012d2e1433d13757
SAM LAW <sam@mrsamlaw.com>
2024-01-04 09:56:28 +1100

    Minor aesthetic changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,45 +1,46 @@
-"use client";
+'use client';
 
-import { motion, LayoutGroup } from "framer-motion";
-import { usePathname } from "next/navigation";
-import { Suspense } from "react";
-import Link from "next/link";
+import { motion, LayoutGroup } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import { Suspense } from 'react';
+import Link from 'next/link';
 
 const navItems = {
-	"/": {
-		name: "home",
+	'/': {
+		name: 'home',
 	},
-	"/about": {
-		name: "about",
+	'/about': {
+		name: 'about',
 	},
 };
 
 export function Navbar() {
 	return (
-		<aside className="-ml-[8px] mb-16 tracking-tight">
-			<div className="container px-4 mx-auto flex items-center justify-between lg:sticky lg:top-20">
+		<aside className='-ml-[8px] mb-16 tracking-tight'>
+			<div className='container px-4 mx-auto flex items-center justify-between lg:sticky lg:top-20'>
 				<LayoutGroup>
 					<div>
-						<Link href="/" className="text-[40px] text-yellow-400 font-bold">
+						<Link href='/' className='text-[40px] text-yellow-400 font-bold'>
 							CHONGLAND
 						</Link>
-						<div className="text-sm">
+						<div className='text-sm'>
 							&ldquo;
-							<span className="italic">
+							<span className='italic'>
 								...live separately from the rest of the people in the city
 							</span>
 							&rdquo;
+							<br />
 							<Link
-								href="https://www.urbandictionary.com/define.php?term=Chongland"
-								className="text-yellow-400"
+								href='https://www.urbandictionary.com/define.php?term=Chongland'
+								className='text-yellow-400'
 							>
 								Urban Dictionary
 							</Link>
 						</div>
 					</div>
 					<nav
-						className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-p-6 md:relative"
-						id="nav"
+						className='flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-p-6 md:relative'
+						id='nav'
 					>
 						<Suspense fallback={null}>
 							{Object.entries(navItems).map(([path, { name }]) => {
@@ -56,9 +57,9 @@ export function Navbar() {
 //let cx = (...classes) => classes.filter(Boolean).join(" ");
 
 function NavItem({ path, name }: { path: string; name: string }) {
-	let pathname = usePathname() || "/";
-	if (pathname.includes("/blog/")) {
-		pathname = "/blog";
+	let pathname = usePathname() || '/';
+	if (pathname.includes('/blog/')) {
+		pathname = '/blog';
 	}
 	let isActive = path === pathname;
 
@@ -66,20 +67,20 @@ function NavItem({ path, name }: { path: string; name: string }) {
 		<Link
 			key={path}
 			href={path}
-			className="transition-all flex align-middle text-[15px] tracking-[2px] uppercase font-semibold pb-2"
+			className='transition-all flex align-middle text-[15px] tracking-[2px] uppercase font-semibold pb-2'
 			/*				{
 					"text-neutral-50": !isActive,
 				}*/
 		>
-			<span className="relative py-1 px-2">
+			<span className='relative py-1 px-2'>
 				{name}
 				{path === pathname ? (
 					<motion.div
-						className="absolute h-[5px] top-7 mx-2 inset-0 bg-yellow-400 z-[-1]"
+						className='absolute h-[5px] top-7 mx-2 inset-0 bg-yellow-400 z-[-1]'
 						/*className="relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-[2px] before:bg-yellow-500 before:origin-[100%, 50%] before:transition-all before:duration-300 before:ease-in-out before:scale-x-0 before:scale-y-[1] before:scale-z-[1] before:will-change-transform hover:before:origin-[100&, 0%] hover:before:scale-x-[1] hover:before:scale-y-[1] hover:before:scale-z-[1] text-[12px] tracking-[2px] uppercase font-semibold pb-2"*/
-						layoutId="sidebar"
+						layoutId='sidebar'
 						transition={{
-							type: "spring",
+							type: 'spring',
 							stiffness: 350,
 							damping: 30,
 						}}
--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,16 +1,22 @@
 export function formatDate(date: string) {
 	let currentDate = new Date();
-	if (!date.includes("T")) {
+	if (!date.includes('T')) {
 		date = `${date}T00:00:00`;
 	}
 	let targetDate = new Date(date);
 	let reorderDate = new Date(date);
 
-	let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
-	let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
-	let daysAgo = currentDate.getDate() - targetDate.getDate();
+	//console.log('current ' + currentDate);
+	//console.log('target ' + targetDate);
 
-	let formattedDate = "";
+	let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+	//console.log('years ' + yearsAgo);
+	let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+	//console.log('months ' + monthsAgo);
+	let daysAgo = currentDate.getDate() - targetDate.getDate();
+	//console.log('days ' + daysAgo);
+
+	let formattedDate = '';
 
 	if (yearsAgo > 0) {
 		formattedDate = `${yearsAgo}y ago`;
@@ -19,34 +25,34 @@ export function formatDate(date: string) {
 	} else if (daysAgo > 0) {
 		formattedDate = `${daysAgo}d ago`;
 	} else {
-		formattedDate = "Today";
+		formattedDate = 'Today';
 	}
 
 	const month = [
-		"January ",
-		"February ",
-		"March ",
-		"April ",
-		"May ",
-		"June ",
-		"July ",
-		"August ",
-		"September ",
-		"October ",
-		"November ",
-		"December ",
+		'January ',
+		'February ',
+		'March ',
+		'April ',
+		'May ',
+		'June ',
+		'July ',
+		'August ',
+		'September ',
+		'October ',
+		'November ',
+		'December ',
 	];
 
 	let RDate =
 		month[reorderDate.getMonth()] +
 		reorderDate.getDate() +
-		", " +
+		', ' +
 		reorderDate.getFullYear();
 
-	let fullDate = targetDate.toLocaleString("en-au", {
-		month: "long",
-		day: "numeric",
-		year: "numeric",
+	let fullDate = targetDate.toLocaleString('en-au', {
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric',
 	});
 	return `${RDate} - [${formattedDate}]`;
 }
